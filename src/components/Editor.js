5ec30b2fe39b50d5260e71c1803066d5fdd3dd01
k2maan <ketuman333@gmail.com>
2020-11-29 21:05:46 +0530

    added basic functionality

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,29 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/mode/xml/xml";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
 import { Controlled as ControlledEditor } from "react-codemirror2";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 export default function Editor(props) {
   const { language, displayName, value, onChange } = props;
+  const [open, setOpen] = useState(true);
 
   function handleChange(editor, data, value) {
     onChange(value);
   }
 
   return (
-    <div className="editor-container">
+    <div className={`editor-container ${open ? "" : "collapsed"}`}>
       <div className="editor-title">
         {displayName}
-        <button>O/C</button>
+        <button
+          type="button"
+          className="expand-collapse-btn"
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
+        >
+          <FontAwesomeIcon icon={open ? faArrowLeft : faArrowRight} />
+        </button>
       </div>
-
       <ControlledEditor
         onBeforeChange={handleChange}
         value={value}
-        className="code-mirror=wrapper"
+        className="code-mirror-wrapper"
         options={{
           lineWrapping: true,
           lint: true,
